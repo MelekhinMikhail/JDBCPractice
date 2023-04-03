c5d126a8a698a509967acef5817e7d7f405fea50
Admin <admin@mail.ru>
2023-04-03 22:13:30 +0300

    Task 2

---
+++ src/main/java/org/example/dao/EmployeeDAOJDBC.java
@@ -0,0 +1,132 @@
+package org.example.dao;
+
+import org.example.models.Employee;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class EmployeeDAOJDBC implements EmployeeDAO {
+    @Override
+    public void addEmployee(Employee employee) {
+        String query = "INSERT INTO employee(first_name, last_name, gender, age, city_id) " +
+                "VALUES(?, ?, ?, ?, ?)";
+
+        try {
+            Connection connection = getConnection();
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, employee.getFirstName());
+            statement.setString(2, employee.getLastName());
+            statement.setString(3, employee.getGender());
+            statement.setInt(4, employee.getAge());
+            statement.setInt(5, employee.getCityId());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Optional<Employee> getEmployee(int id) {
+        String query = "SELECT * FROM employee WHERE id=?";
+
+        try {
+            Connection connection = getConnection();
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            resultSet.next();
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String gender = resultSet.getString("gender");
+            Integer age = resultSet.getInt("age");
+            Integer cityId = resultSet.getInt("city_id");
+
+            Employee employee = new Employee(id, firstName, lastName, gender, age, cityId);
+            return Optional.of(employee);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        String query = "SELECT * FROM employee";
+        List<Employee> list = new ArrayList<>();
+
+        try {
+            Connection connection = getConnection();
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String gender = resultSet.getString("gender");
+                Integer age = resultSet.getInt("age");
+                Integer cityId = resultSet.getInt("city_id");
+
+                list.add(new Employee(id, firstName, lastName, gender, age, cityId));
+            }
+
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void updateEmployee(int id, Employee employee) {
+        String query = "UPDATE employee SET first_name=?, last_name=?, gender=?, age=?, city_id=? WHERE id=?";
+
+        try {
+            Connection connection = getConnection();
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, employee.getFirstName());
+            statement.setString(2, employee.getLastName());
+            statement.setString(3, employee.getGender());
+            statement.setInt(4, employee.getAge());
+            statement.setInt(5, employee.getCityId());
+            statement.setInt(6, id);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        String query = "DELETE FROM employee WHERE id=?";
+
+        try {
+            Connection connection = getConnection();
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Connection getConnection() throws SQLException {
+        final String url = "jdbc:postgresql://localhost:5432/test";
+        final String user = "postgres";
+        final String password = "1234567890";
+
+        return DriverManager.getConnection(url, user, password);
+    }
+}
--- src/main/java/org/example/models/Employee.java
+++ src/main/java/org/example/models/Employee.java
@@ -2,15 +2,23 @@ package org.example.models;
 
 public class Employee {
 
-    private int id;
+    private Integer id;
     private String firstName;
     private String lastName;
     private String gender;
     private Integer age;
     private Integer cityId;
 
-    public Employee(int id, String firstName, String lastName, String gender, Integer age, Integer cityId) {
+    public Employee(Integer id, String firstName, String lastName, String gender, Integer age, Integer cityId) {
         this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.cityId = cityId;
+    }
+
+    public Employee(String firstName, String lastName, String gender, Integer age, Integer cityId) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
@@ -28,5 +36,53 @@ public class Employee {
                 ", age=" + age +
                 ", cityId=" + cityId +
                 '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
     }
 }
